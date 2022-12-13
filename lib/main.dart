5bd8279e70c4c08df792c4faf5630f37f81dd4de
Omare Faruk <ofaruk0169@gmail.com>
2022-12-13 16:55:20 +0000

    added a better layout using Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  build(context) {}
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('My first app')),
+        body: Text('This is my default text!'),
+      ),
+    );
+  }
 }
