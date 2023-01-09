4a678610569403bf8147f6e7f612c4cc5da88088
Omare Faruk <ofaruk0169@gmail.com>
2023-01-09 18:00:14 +0000

    made a change in buttons

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,37 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('answer chosen yay');
+  }
+
   Widget build(BuildContext context) {
+    var questions = [
+      'What is your favorite color?',
+      'What is your favortie animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('My first app')),
-        body: Text('This is my default text! I have not coded forever'),
+        appBar: AppBar(
+          title: Text('My first app'),
+        ),
+        body: Column(
+          children: [
+            Text('The question'),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
